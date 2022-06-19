464102a8fce033046239a0f12de5efb562dfa9ba
a2n-s <44101798+AntoineStevan@users.noreply.github.com>
2022-06-19 14:08:33 +0200

    (feat) remove .SRCINFO deletion on cleanup

--- cleanup.sh
+++ cleanup.sh
@@ -7,5 +7,5 @@ full_path=$(realpath "$0")
 dir_path=${full_path%/*}
 
 find "$dir_path/x86_64" -mindepth 2 -maxdepth 2 -type d | xargs -I {} rm -rfv {}
-find "$dir_path/x86_64" -maxdepth 2 -type f -not -name PKGBUILD -not -name "*.install" | xargs -I {} rm -vf {}
+find "$dir_path/x86_64" -maxdepth 2 -type f -not -name PKGBUILD -not -name "*.install" -not -name ".SRCINFO" | xargs -I {} rm -vf {}
 echo "$dir_path is now clean!"
